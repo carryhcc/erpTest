f43223851e342e616b54699eb378aade83cc4aef
cchu <hcc_sf@jztey.com>
2022-02-09 15:59:08 +0800

    :art: 重新格式化代码

--- src/main/java/com/example/config/MybatisPlusConfig.java
+++ src/main/java/com/example/config/MybatisPlusConfig.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -11,13 +12,14 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 /**
  * Created by IntelliJ IDEA.
  * Spring boot方式
- *
+ * EnableTransactionManagement  自动管理事务
+ * Configuration  配置类注解
  * @author : cchu
  * Date: 2022/1/6 11:14
  */
-
-@EnableTransactionManagement    // 自动管理事务
-@Configuration  // 配置类注解
+@MapperScan("com.example.mapper")
+@EnableTransactionManagement
+@Configuration
 public class MybatisPlusConfig {
 
 
@@ -27,6 +29,7 @@ public class MybatisPlusConfig {
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        //默认数据库类型
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.ORACLE));
         //乐观锁
         interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
